47dbce347f4fd7690fa443027d5683af9de019f3
Sahil <944sahil@gmail.com>
2024-06-30 22:55:44 +0530

    Server set up

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js"
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const PORT=process.env.PORT || 5000;
+const app=express();
+
+app.use("/api/auth",authRoutes);
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send('Hello from Express Server');
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+});
