fb993f91560fdd0897b6268be72ef86782fffcf4
Sahil <944sahil@gmail.com>
2024-07-18 11:45:17 +0530

    minor changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
 
 export const signup =async (req,res)=>{
     try {
-        const{fullname,username,email,password}=req.body;
+        const{fullName,username,email,password}=req.body;
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if(!emailRegex.test(email)){
             return res.status(400).json({error: "Invalid email format"});
@@ -24,7 +24,7 @@ export const signup =async (req,res)=>{
         const hashedPassword = await bcrypt.hash(password,salt);
 
         const newUser=new User({
-            fullname,
+            fullName,
             username,
             email,
             password:hashedPassword,
@@ -36,7 +36,7 @@ export const signup =async (req,res)=>{
 
             res.status(201).json({
                 _id:newUser._id,
-                fullname: newUser.fullname,
+                fullName: newUser.fullName,
                 username: newUser.username,
                 email: newUser.email,
                 followers: newUser.followers,
@@ -66,7 +66,7 @@ export const login =async (req,res)=>{
 
         res.status(200).json({
             _id: user._id,
-            fullname: user.fullname,
+            fullName: user.fullName,
             username: user.username,
             email: user.email,
             followers: user.followers,
