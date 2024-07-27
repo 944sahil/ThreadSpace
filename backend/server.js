3f083b3887cdd078ea939455211871d319b86038
Sahil <944sahil@gmail.com>
2024-07-27 10:04:38 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
@@ -22,7 +22,7 @@ const PORT=process.env.PORT || 5000;
 const app=express();
 
 
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
