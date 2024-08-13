b998f59eb2e8e6b185041a6934ee19482a6d3307
Sahil <944sahil@gmail.com>
2024-08-14 04:52:04 +0530

    like post mutation added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -71,8 +71,10 @@ export const likeUnlikePost = async(req,res)=>{
             //unlike post
             await Post.updateOne({_id:postId},{$pull: {likes:userId}});
             await User.updateOne({_id:userId},{$pull: {likedPosts:postId}});
+
+            const updatedLikes = post.likes.filter((id)=> id.toString()!== userId.toString());
             
-            res.status(200).json({msg: "Post unliked successfully."});
+            res.status(200).json(updatedLikes);
         } else {
             //like post
             post.likes.push(userId);
@@ -88,7 +90,8 @@ export const likeUnlikePost = async(req,res)=>{
             
             await notification.save();
 
-            res.status(200).json({msg: "Post liked successfully."});
+            const updatedLikes=post.likes;
+            res.status(200).json(updatedLikes);
         }
 
     } catch (error) {
